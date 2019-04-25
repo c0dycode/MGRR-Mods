efbf7dd3c026777d20a8733c7e68e82f6b000614
c0dycode <schulten_705@hotmail.com>
2019-04-25 22:56:16 +0200

    Split into header and cpp file

--- MGR/MGR.h
+++ MGR/MGR.h
@@ -1,5 +1,8 @@
 #pragma once
 #pragma pack(0x4)
+#ifndef MGR_H
+#define MGR_H
+
 #include <Windows.h>
 #include <cstdint>
 #include <random>
@@ -856,108 +859,7 @@ public:
 }; //Size: 0x0040
 
 
-void RandomizeRaiden(PTenThousand* pTenK) {
-	static std::default_random_engine e;
-	static std::uniform_real_distribution<> dis(-2.5, 2.5);
-	auto partCollection = pTenK->RaidenPartCollection;
-	if (partCollection){
-#pragma region Chest
-		if (partCollection->Chest) {
-			partCollection->Chest->X = dis(e);
-			partCollection->Chest->Y = dis(e);
-			partCollection->Chest->Z = dis(e);
-			partCollection->Chest->Freeze = 8;
-		}
-#pragma endregion
-#pragma region Chest2
-		if (partCollection->Chest2) {
-			partCollection->Chest2->X = dis(e);
-			partCollection->Chest2->Y = dis(e);
-			partCollection->Chest2->Z = dis(e);
-			partCollection->Chest2->Freeze = 8;
-		}
-#pragma endregion
-#pragma region Chest3
-		if (partCollection->Chest3) {
-			partCollection->Chest3->X = dis(e);
-			partCollection->Chest3->Y = dis(e);
-			partCollection->Chest3->Z = dis(e);
-			partCollection->Chest3->Freeze = 8;
-		}
-#pragma endregion
+void RandomizeRaiden(PTenThousand* pTenK);
+void UnRandomizeRaiden(PTenThousand* pTenK);
 
-#pragma region Legs
-		if (partCollection->LeftLowerLeg) {
-			partCollection->LeftLowerLeg->X = dis(e);
-			partCollection->LeftLowerLeg->Y = dis(e);
-			partCollection->LeftLowerLeg->Freeze = 8;
-		}
-		if (partCollection->RightLowerLeg) {
-			partCollection->RightLowerLeg->X = dis(e);
-			partCollection->RightLowerLeg->Y = dis(e);
-			partCollection->RightLowerLeg->Freeze = 8;
-		}
-#pragma endregion
-
-#pragma region Head
-		if (partCollection->Head2) {
-			partCollection->Head2->X = dis(e);
-			partCollection->Head2->Y = dis(e);
-			partCollection->Head2->Z = dis(e);
-			partCollection->Head2->Freeze = dis(e);
-		}
-#pragma endregion
-	}	
-}
-
-void UnRandomizeRaiden(PTenThousand* pTenK) {
-	auto partCollection = pTenK->RaidenPartCollection;
-	if (partCollection) {
-#pragma region Chest
-		if (partCollection->Chest) {
-			partCollection->Chest->X = 1.0f;
-			partCollection->Chest->Y = 1.0f;
-			partCollection->Chest->Z = 1.0f;
-			partCollection->Chest->Freeze = 0;
-		}
-#pragma endregion
-#pragma region Chest2
-		if (partCollection->Chest2) {
-			partCollection->Chest2->X = 1.0f;
-			partCollection->Chest2->Y = 1.0f;
-			partCollection->Chest2->Z = 1.0f;
-			partCollection->Chest2->Freeze = 0;
-		}
-#pragma endregion
-#pragma region Chest3
-		if (partCollection->Chest3) {
-			partCollection->Chest3->X = 1.0f;
-			partCollection->Chest3->Y = 1.0f;
-			partCollection->Chest3->Z = 1.0f;
-			partCollection->Chest3->Freeze = 0;
-		}
-#pragma endregion
-
-#pragma region Legs
-		if (partCollection->LeftLowerLeg) {
-			partCollection->LeftLowerLeg->X = 1.0f;
-			partCollection->LeftLowerLeg->Y = 1.0f;
-			partCollection->LeftLowerLeg->Freeze = 0;
-		}
-		if (partCollection->RightLowerLeg) {
-			partCollection->RightLowerLeg->X = 1.0f;
-			partCollection->RightLowerLeg->Y = 1.0f;
-			partCollection->RightLowerLeg->Freeze = 0;
-		}
-#pragma endregion
-
-#pragma region Head
-		if (partCollection->Head2) {
-			partCollection->Head2->X = 1.0f;
-			partCollection->Head2->Y = 1.0f;
-			partCollection->Head2->Z = 1.0f;
-			partCollection->Head2->Freeze = 0;
-		}
-#pragma endregion
-	}
-}
+#endif
