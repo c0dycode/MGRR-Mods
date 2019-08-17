97b80eee310791ea338d33049d11acb427e68d54
c0dycode <salx92@gmail.com>
2019-08-17 04:06:52 +0200

    Added enum for CurrentCharacter, movementSpeed Multiplier and some stuff for future research help

--- MGR/GlobalVariables.h
+++ MGR/GlobalVariables.h
@@ -2,14 +2,17 @@
 #define GLOBALVARIABLES_H
 #include "MGR.h"
 
-extern uintptr_t BaseAddress;
-extern PTenThousand* P10000;
-extern PTenThousand* LastTargetedEnemy;
-extern PTenThousand* PlWig;
-extern PTenThousand* PlSwordSheath;
+extern uintptr_t		BaseAddress;
+extern PTenThousand*	P10000;
+extern PTenThousand*	LastTargetedEnemy;
+extern PTenThousand*	PlWig;
+extern PTenThousand*	PlSwordSheath;
+extern void*			CustomizeSelMenu;
+extern void*			ObjReadSystem;
 
 extern PlayerManager* PManager;
 extern VRGBA* vRaidenColors;
+extern ECharacter CurrentCharacter;
 
 extern bool bShowOverlay;
 extern bool bDisableTakingDamage;
@@ -17,6 +20,7 @@ extern bool bHaveOriginalVMT;
 extern bool bColorsNeedRefresh;
 
 extern int currency;
+extern int movementSpeedMultiplier;
 //extern int iColorsChoice;
 
 extern float fCameraHeigthOffset;
