37b2c2de4a1b54453d200314a00b4e76bdd4870a
c0dycode <salx92@gmail.com>
2019-08-17 04:07:47 +0200

    Added ECharacter-Enum and updated/changed a bunch of classes

--- MGR/MGR.h
+++ MGR/MGR.h
@@ -1,5 +1,5 @@
 #pragma once
-#pragma pack(0x4)
+//#pragma pack(0x4)
 #ifndef MGR_H
 #define MGR_H
 
@@ -41,6 +41,13 @@ struct Vector4 {
 	float Y;
 	float Z;
 	float A;
+};
+
+enum ECharacter {
+	None = 0,
+	Raiden = 1,
+	Sam = 2,
+	Wolf = 3
 };
 
 extern std::map<std::string, VRGBA*> colorMap;
@@ -338,16 +345,23 @@ public:
 class cPart
 {
 public:
-	char pad_0004[108]; //0x0004
+	char pad_0004[76]; //0x0004
+	float ModelPositionY; //0x0050
+	float ModelPositionX; //0x0054
+	float ModelPositionZ; //0x0058
+	char pad_005C[20]; //0x005C
 	float X; //0x0070
 	float Y; //0x0074
 	float Z; //0x0078
-	char pad_007C[36]; //0x007C
+	char pad_007C[24]; //0x007C
+	float ModelRotation; //0x0094
+	float ModelLeaning; //0x0098
+	char pad_009C[4]; //0x009C
 	uint16_t PartIndex; //0x00A0
 	uint16_t Freeze; //0x00A2
 	char pad_00A4[12]; //0x00A4
 
-	virtual void Function0();
+	virtual void Destructor();
 	virtual void Function1();
 	virtual void Function2();
 	virtual void Function3();
@@ -590,7 +604,7 @@ public:
 	virtual void Function240();
 	virtual void Function241();
 	virtual void HandleUserInput();
-	virtual void TickQuestionMark();
+	virtual void Tick();
 	virtual void Function244();
 	virtual void Function245();
 	virtual void Function246();
@@ -744,11 +758,28 @@ public:
 	char pad_0000[128]; //0x0000
 }; //Size: 0x0080
 
-class PTenThousand : public cPart
+class PTenThousand
 {
 public:
-	class N000031DB ModelBaseQuestionmark; //0x00B0
-	char pad_0130[496]; //0x0130
+	char pad_0004[76]; //0x0004
+	float ModelPositionY; //0x0050
+	float ModelPositionX; //0x0054
+	float ModelPositionZ; //0x0058
+	char pad_005C[20]; //0x005C
+	float X; //0x0070
+	float Y; //0x0074
+	float Z; //0x0078
+	char pad_007C[24]; //0x007C
+	float ModelRotation; //0x0094
+	float ModelLeaning; //0x0098
+	char pad_009C[4]; //0x009C
+	uint16_t PartIndex; //0x00A0
+	uint16_t Freeze; //0x00A2
+	char pad_00A4[400]; //0x00A4
+	class N00005D4E* BaseArmorPointer; //0x0234
+	char pad_0238[216]; //0x0238
+	uint32_t bEnableBodyStealth; //0x0310
+	char pad_0314[12]; //0x0314
 	class cMesh* Meshes; //0x0320
 	uint32_t MeshesCount; //0x0324
 	char pad_0328[20]; //0x0328
@@ -756,20 +787,43 @@ public:
 	char pad_0340[20]; //0x0340
 	class RaidenParts* PartCollection; //0x0354
 	uint32_t PartCount; //0x0358
-	char pad_035C[1100]; //0x035C
+	char pad_035C[268]; //0x035C
+	uint32_t OwnerObjID; //0x0468
+	char pad_046C[68]; //0x046C
+	uint32_t ObjectID; //0x04B0
+	uint32_t N00002FC0; //0x04B4
+	char pad_04B8[4]; //0x04B8
+	uint32_t N000002EC; //0x04BC
+	char pad_04C0[36]; //0x04C0
+	uint32_t CameraMode; //0x04E4
+	char pad_04E8[304]; //0x04E8
+	uint16_t CurrentInput; //0x0614
+	uint16_t Unk; //0x0618
+	char pad_061C[328]; //0x061C
+	class N00005D1F* CharacterProxies; //0x0764
+	char pad_0768[64]; //0x0768
 	class N00007278* CollisionArrayData; //0x07A8
 	char pad_07AC[12]; //0x07AC
 	class AllocatedArray* CollisionArray; //0x07B8
 	char pad_07BC[180]; //0x07BC
 	uint32_t CurrentHP; //0x0870
-	uint32_t MaxHP; //0x0874
-	char pad_0878[3708]; //0x0878
+	uint32_t BaseHP; //0x0874
+	uint32_t N000003D7; //0x0878
+	char pad_087C[760]; //0x087C
+	uint32_t bHideWeaponEffect; //0x0B74
+	char pad_0B78[2184]; //0x0B78
+	uint32_t bHideWeapon; //0x1400
+	char pad_1404[12]; //0x1404
+	uint32_t N00001431; //0x1410
+	char pad_1414[736]; //0x1414
 	uint32_t HidePrimaryWeapon; //0x16F4
 	char pad_16F8[9308]; //0x16F8
 	uint32_t CanNotSprint; //0x3B54
 	char pad_3B58[76]; //0x3B58
 	uint32_t bIsBladeModeActive; //0x3BA4
-	char pad_3BA8[1324]; //0x3BA8
+	char pad_3BA8[1316]; //0x3BA8
+	int32_t bIsSprinting; //0x40CC
+	char pad_40D0[4]; //0x40D0
 	class N000051D4* N00001BE3; //0x40D4
 	char pad_40D8[152]; //0x40D8
 	uint32_t bIsAutoClimbing; //0x4170
@@ -778,28 +832,28 @@ public:
 	char pad_41B4[4]; //0x41B4
 	float N00001C1C; //0x41B8
 	char pad_41BC[516]; //0x41BC
-	uint32_t bIsSprinting; //0x43C0
+	uint32_t bIsSprinting2; //0x43C0
 	char pad_43C4[4016]; //0x43C4
 	uint32_t bCanSprint; //0x5374
-	char pad_5378[136]; //0x5378
+	char pad_5378[1048]; //0x5378
 
-	virtual void Function0();
-	virtual void Function1();
-	virtual void Function2();
+	virtual void Destructor(char param);
+	virtual void* GetSomething();
+	virtual bool SomethingWithBXMWTBAndMore();
 	virtual void Function3();
-	virtual void Function4();
-	virtual void Function5();
-	virtual void Function6();
+	virtual void ReturnFunc0();
+	virtual void ReturnFunc1();
+	virtual void SomeThunk();
 	virtual void Function7();
 	virtual void Function8();
-	virtual void Function9();
-	virtual void Function10();
-	virtual void Function11();
+	virtual float GetFloatOne();
+	virtual void SetSomeValue(DWORD valueToSet);
+	virtual void ReturnFunc2();
 	virtual void Function12();
 	virtual void Function13();
 	virtual void Function14();
-	virtual void AccessesSomeStaticArray();
-	virtual void BigFuncLotsOfFloats();
+	virtual void AccessSomeStaticArray();
+	virtual void LoadRaiden();
 	virtual void Function17();
 	virtual void Function18();
 	virtual void Function19();
@@ -851,9 +905,9 @@ public:
 	virtual void Function65();
 	virtual void Function66();
 	virtual void Function67();
-	virtual void Function68();
-	virtual void Function69();
-	virtual void Function70();
+	virtual void SetBodyStealth();
+	virtual void MaybeCustomizationMenuCtor();
+	virtual bool CustomizationMenu(int param = 0);
 	virtual void Function71();
 	virtual void Function72();
 	virtual void Function73();
@@ -866,16 +920,16 @@ public:
 	virtual void Function80();
 	virtual void Function81();
 	virtual void Function82();
-	virtual void Function83();
+	virtual bool Function83(int param_2, int param_3);
 	virtual void Function84();
 	virtual void Function85();
-	virtual void Function86();
+	virtual bool Function86(int param_2);
 	virtual void Function87();
-	virtual void Function88();
-	virtual void Function89();
-	virtual void Function90();
+	virtual void Function88(int param_2);
+	virtual bool Function89();
+	virtual bool Function90(int param_2, int param_3);
 	virtual void Function91();
-	virtual void Function92();
+	virtual bool ReturnTrue();
 	virtual void Function93();
 	virtual void Function94();
 	virtual void Function95();
@@ -906,8 +960,8 @@ public:
 	virtual void Function120();
 	virtual void Function121();
 	virtual void Function122();
-	virtual void Function123();
-	virtual void Function124();
+	virtual void SomethingWhenPreviewingDifferentArmor1();
+	virtual void SomethingWhenPreviewingDifferentArmor2();
 	virtual void Function125();
 	virtual void Function126();
 	virtual void Function127();
@@ -930,7 +984,7 @@ public:
 	virtual void Function144();
 	virtual void Function145();
 	virtual void Function146();
-	virtual void Function147();
+	virtual void SomethingWhenPreviewingDifferentArmor3();
 	virtual void Function148();
 	virtual void Function149();
 	virtual void Function150();
@@ -976,10 +1030,10 @@ public:
 	virtual void Function190();
 	virtual void Function191();
 	virtual void Function192();
-	virtual void Function193();
+	virtual void SomethingWhenPreviewingDifferentArmor4();
 	virtual void Function194();
-	virtual void TakeDamage();
-	virtual void IncreaseHP();
+	virtual void TakeDamage(DWORD param1, DWORD param2);
+	virtual void IncreaseHP(int incHP);
 	virtual void Function197();
 	virtual void Function198();
 	virtual void Function199();
@@ -989,7 +1043,7 @@ public:
 	virtual void Function203();
 	virtual void Function204();
 	virtual void Function205();
-	virtual void Function206();
+	virtual void CanPlayerMove();
 	virtual void Function207();
 	virtual void Function208();
 	virtual void Function209();
@@ -1010,11 +1064,11 @@ public:
 	virtual void Function224();
 	virtual void ShowPrimaryWeapon();
 	virtual void Function226();
-	virtual void Function227();
+	virtual void MaybeInstaKillPlayer();
 	virtual void Function228();
 	virtual void Function229();
 	virtual void Function230();
-	virtual void Function231();
+	virtual void CalledOnArmorSwitch();
 	virtual void Function232();
 	virtual void Function233();
 	virtual void Function234();
@@ -1029,17 +1083,15 @@ public:
 	virtual void Function243();
 	virtual void Function244();
 	virtual void Function245();
-	virtual void Function246();
+	virtual void CalledOnArmorSwitch2();
 	virtual void Function247();
-	virtual void Function248();
+	virtual void CalledOnArmorSwitch3();
 	virtual void Function249();
 	virtual void Function250();
 	virtual void Function251();
 	virtual void Function252();
 	virtual void Function253();
-}; //Size: 0x58F4
-
-
+}; //Size: 0x5790
 
 class N00000E41
 {
